dd01b4f9d0e6adb4c56ee32f6f6571639fda556e
44jovi <kwong.jovi@gmail.com>
2023-04-21 12:28:09 +0100

    pass test testCreate (before tickboxes checking)

--- src/test/java/com/qa/demo/selenium/SpringSeleniumTest.java
+++ src/test/java/com/qa/demo/selenium/SpringSeleniumTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -63,6 +64,29 @@ public class SpringSeleniumTest {
 				.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#output > div > div > div")));
 
 		Assertions.assertTrue(card.getText().contains("Mr Bigglesworth"));
+	}
+
+	@Test
+	void testCreate() {
+		this.driver.get("http://localhost:" + port);
+
+		WebElement catName = this.driver.findElement(By.cssSelector("#catName"));
+		catName.sendKeys("blah");
+
+		WebElement catLength = this.driver.findElement(By.cssSelector("#catLength"));
+		catLength.sendKeys("1");
+
+		// TODO: tick Whiskers
+		// TODO: tick Evil
+
+		WebElement submitBtn = this.driver.findElement(By.cssSelector("#catForm > div.mt-3 > button.btn.btn-success"));
+		submitBtn.sendKeys(Keys.ENTER);
+
+		WebElement card = this.wait
+				.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#output > div:nth-child(2) > div > div")));
+
+		Assertions.assertTrue(card.getText().contains("blah"));
+
 	}
 
 }
